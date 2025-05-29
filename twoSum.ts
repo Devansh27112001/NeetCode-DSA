cfe1364c239b1caa00b46e3677267cac4cd83b9e
Devansh27112001 <dkansara2711@gmail.com>
2025-05-28 20:15:49 -0400

    twoSum: TWO POINTER APPROACH

--- twoSum.ts
+++ twoSum.ts
@@ -9,3 +9,30 @@ function twoSumBrute(nums: Array<number>, target: number): Array<number> {
   }
   return [];
 }
+
+// TWO POINTERS
+function twoSumPointers(nums: Array<number>, target: number): Array<number> {
+  const tempArray: Array<[number, number]> = [];
+
+  // Pushing the [value,index]
+  for (let i = 0; i < nums.length; i++) {
+    tempArray.push([nums[i], i]);
+  }
+
+  // Sorting the array according to value.
+  tempArray.sort((a, b) => a[0] - b[0]);
+  let low: number = 0;
+  let high: number = nums.length - 1;
+
+  while (low < high) {
+    let currTotal = tempArray[low][0] + tempArray[high][0];
+    if (currTotal === target) {
+      return [tempArray[low][1], tempArray[high][1]];
+    } else if (currTotal < target) {
+      low++;
+    } else {
+      high--;
+    }
+  }
+  return [];
+}
