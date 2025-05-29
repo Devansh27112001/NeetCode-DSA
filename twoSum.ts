62efe5b1912bbf359cd743145a7c2659d74aed2c
Devansh27112001 <dkansara2711@gmail.com>
2025-05-28 20:06:33 -0400

    Brute Force approach

---
+++ twoSum.ts
@@ -0,0 +1,11 @@
+// BRUTE FORCE
+function twoSumBrute(nums: Array<number>, target: number): Array<number> {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
