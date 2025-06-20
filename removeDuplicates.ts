5693609d029c6d94b9a72be0c49e2b7f46644108
Devansh27112001 <dkansara2711@gmail.com>
2025-06-20 12:26:14 -0400

    removing duplicates in-place from a sorted array

---
+++ removeDuplicates.ts
@@ -0,0 +1,10 @@
+const removeDuplicates = (nums: Array<number>): number => {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      nums[i + 1] = j;
+      i++;
+    }
+  }
+  return i + 1;
+};
