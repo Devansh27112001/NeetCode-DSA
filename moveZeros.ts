85ee9e23456cc298476e2d241bb4b0d9c3be3cbc
Devansh27112001 <dkansara2711@gmail.com>
2025-06-23 22:38:49 -0400

    Move zeros --optimized

--- moveZeros.ts
+++ moveZeros.ts
@@ -16,3 +16,36 @@ const moveZeros = (nums: Array<number>): Array<number> => {
   }
   return nums;
 };
+
+// This will not preserve order of the non-zero elements
+const moveZerosTwo = (nums: Array<number>): Array<number> => {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low < high) {
+    if (nums[low] === 0) {
+      if (nums[high] !== 0) {
+        [nums[low], nums[high]] = [nums[high], nums[low]];
+        low++;
+        high--;
+      } else {
+        high--;
+      }
+    } else {
+      low++;
+    }
+  }
+
+  return nums;
+};
+
+const moveZerosTwoPreserveOrder = (nums: Array<number>): Array<number> => {
+  let low = 0;
+  for (let high = 0; high < nums.length; high++) {
+    if (nums[high] !== 0) {
+      [nums[low], nums[high]] = [nums[high], nums[low]];
+      low++;
+    }
+  }
+  return nums;
+};
