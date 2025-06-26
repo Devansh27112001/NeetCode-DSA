7284f8e8287a744d76f5468398bd5dc290a7b6c5
Devansh27112001 <dkansara2711@gmail.com>
2025-06-26 18:02:23 -0400

    intersection array --optimized --similar_to_union

--- unionAndIntersection.ts
+++ unionAndIntersection.ts
@@ -1,3 +1,5 @@
+// TC : O(n + m)
+// SC : O(n + m) => Space used by the unionArray that is returned from the function.
 const unionArrays = (
   arr_1: Array<number>,
   arr_2: Array<number>
@@ -38,4 +40,35 @@ const unionArrays = (
   return arrUnion;
 };
 
-console.log(unionArrays([1, 1, 2, 3, 4, 4, 5], [2, 3, 4, 5, 6]));
+// console.log(unionArrays([1, 1, 2, 3, 4, 4, 5], [2, 3, 4, 5, 6]));
+
+const intersectionArrays = (
+  a: Array<number>,
+  b: Array<number>
+): Array<number> => {
+  const intersecArray: Array<number> = [];
+  const n = a.length;
+  const m = b.length;
+  let i = 0;
+  let j = 0;
+
+  while (i < n && j < m) {
+    if (a[i] === b[j]) {
+      if (
+        intersecArray.length === 0 ||
+        intersecArray[intersecArray.length - 1] !== a[i]
+      ) {
+        intersecArray.push(a[i]);
+      }
+      i++;
+      j++;
+    } else if (a[i] < b[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return intersecArray;
+};
+
+console.log(intersectionArrays([1, 1, 2, 3, 4, 4, 5], [2, 3, 4, 5, 6]));
