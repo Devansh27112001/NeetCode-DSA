3d6b1613e4ac2164cb6aa0a9c7660fbb9d8c8ddb
Devansh27112001 <dkansara2711@gmail.com>
2025-06-28 23:34:01 -0400

    missing number --optimized

--- missingNumber.ts
+++ missingNumber.ts
@@ -30,26 +30,24 @@ const missingNumber_Set = (nums: Array<number>): number => {
   return -1;
 };
 
-// VERY CLEVER WAY TO DO IT
-const missingNumber_math = (nums: Array<number>): number => {
-  const num_length: number = nums.length;
-  const sum = (num_length * (num_length + 1)) / 2;
-  const nums_sum: number = nums.reduce((acc, curr) => acc + curr, 0);
-
-  return sum - nums_sum;
-};
-
-console.log(missingNumber_math([3, 0, 1]));
-
 const missingNumber_HashArray = (nums: Array<number>) => {
   const hashArray = Array.from({ length: nums.length + 1 }, () => 0);
   for (const item of nums) {
     hashArray[item] = 1;
   }
 
-  console.log(hashArray);
   const missing = hashArray.findIndex((element) => element === 0);
   return missing;
 };
 
-console.log(missingNumber_HashArray([3, 0, 1]));
+// VERY CLEVER WAY TO DO IT
+const missingNumber_math = (nums: Array<number>): number => {
+  let num_sum = (nums.length * (nums.length + 1)) / 2;
+  for (const item of nums) {
+    num_sum -= item;
+  }
+
+  return num_sum;
+};
+
+console.log(missingNumber_math([3, 0, 1]));
