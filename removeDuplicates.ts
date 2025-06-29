db244d75aa08e2f5c6daf211b4c7b80b34de0213
Devansh27112001 <dkansara2711@gmail.com>
2025-06-29 19:17:48 -0400

    remove element -optimal --similar to remove duplicates

--- removeDuplicates.ts
+++ removeDuplicates.ts
@@ -2,7 +2,7 @@ const removeDuplicates = (nums: Array<number>): number => {
   let i = 0;
   for (let j = 1; j < nums.length; j++) {
     if (nums[j] !== nums[i]) {
-      nums[i + 1] = j;
+      nums[i + 1] = nums[j];
       i++;
     }
   }
