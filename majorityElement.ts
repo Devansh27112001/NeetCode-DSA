19272b6a13a0540e1f5b1a2a57790414ac7c25d4
Devansh27112001 <dkansara2711@gmail.com>
2025-07-05 23:43:13 -0400

    --brute-force and --better

--- majorityElement.ts
+++ majorityElement.ts
@@ -1,22 +1,32 @@
+// BRUTE FORCE
+// TC: O(n^2)
+const majaorityElement_bruteForce = (nums: Array<number>): number => {
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      nums[i] === nums[j] && count++;
+    }
+    if (count > nums.length / 2) {
+      return nums[i];
+    }
+  }
+  return -1;
+};
+
 // Better
 // TC: O(n)
-const majaorityElement = (nums: Array<number>): number => {
+const majaorityElement_hashMap = (nums: Array<number>): number => {
   let freqMap = new Map<number, number>();
   for (const number of nums) {
     freqMap.set(number, (freqMap.get(number) || 0) + 1);
   }
 
-  console.log(freqMap);
-  let maxFreq = 0;
-
-  let majorityEle: number = 0;
   for (const [key, value] of freqMap.entries()) {
-    if (value > maxFreq) {
-      maxFreq = value;
-      majorityEle = key;
+    if (value > nums.length / 2) {
+      return key;
     }
   }
-  return majorityEle;
+  return -1;
 };
 
-console.log(majaorityElement([2, 2, 1, 1, 1, 2, 2]));
+console.log(majaorityElement_hashMap([2, 2, 1, 1, 1, 2, 2]));
