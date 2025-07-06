b6cb2252c91cc09fe462753b93890a43bfe3f32b
Devansh27112001 <dkansara2711@gmail.com>
2025-07-06 00:44:49 -0400

    --optimal solution

--- majorityElement.ts
+++ majorityElement.ts
@@ -30,3 +30,34 @@ const majaorityElement_hashMap = (nums: Array<number>): number => {
 };
 
 console.log(majaorityElement_hashMap([2, 2, 1, 1, 1, 2, 2]));
+
+// OPTIMAL
+// TC = O(n)
+// SC = O(1)
+const majaorityElement_optimal = (nums: Array<number>): number => {
+  let count = 0;
+  let majEle = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      majEle = nums[i];
+    }
+
+    if (majEle === nums[i]) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  // The below code is needed only ig it is not stated in the problem that a majority element will always exist.
+  count = 0;
+  for (const number of nums) {
+    number === majEle && count++;
+  }
+
+  return count > nums.length / 2 ? majEle : -1;
+};
+
+console.log(
+  majaorityElement_optimal([7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 1, 1, 1, 1])
+);
