37fdf2aab3d0f20e0a85970c4489776bc1dc8dab
Devansh27112001 <dkansara2711@gmail.com>
2025-07-08 14:59:16 -0400

    --brute-force

--- buyAndSellStock.ts
+++ buyAndSellStock.ts
@@ -1,4 +1,19 @@
-const buyAndSell = (nums: Array<number>): number => {
+const buyAndSell_bruteForce = (nums: Array<number>): number => {
+  let profit = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      profit = Math.max(profit, nums[j] - nums[i]);
+    }
+  }
+
+  return profit;
+};
+
+// console.log(buyAndSell_bruteForce([7, 1, 5, 3, 6, 4]));
+// OPTIMAL
+// TC = O(n)
+// SC = O(1)
+const buyAndSell_optimal = (nums: Array<number>): number => {
   let min = nums[0];
   let profit = 0;
   for (let i = 0; i < nums.length; i++) {
