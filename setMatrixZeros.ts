be6f0126aa89f10e1e71c524fc925f864cd5ddab
Devansh27112001 <dkansara2711@gmail.com>
2025-07-13 00:58:57 -0400

    --brute_force

--- setMatrixZeros.ts
+++ setMatrixZeros.ts
@@ -12,15 +12,23 @@ const solution_brute_force = (
       }
     }
   }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (rowZero.has(i) || colZero.has(j)) {
+        arr[i][j] = 0;
+      }
+    }
+  }
   console.log(rowZero, colZero);
+  console.log(arr);
   return -1;
 };
 
 console.log(
   solution_brute_force([
     [1, 1, 1, 1],
-    [1, 0, 0, 1],
-    [1, 1, 0, 1],
+    [1, 0, 1, 1],
+    [1, 1, 1, 1],
     [1, 1, 1, 1],
   ])
 );
