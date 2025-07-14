a07a921b20c342c58505cb3482ff203f65c7ad15
Devansh27112001 <dkansara2711@gmail.com>
2025-07-14 17:21:49 -0400

    --optimal -> In terms of space and --brute_force

--- setMatrixZeros.ts
+++ setMatrixZeros.ts
@@ -1,6 +1,9 @@
+// TC => O(2 * n * m) ~ O(n * m) => n = rows and m = columns
+// SC => O(2 * n * m) ~ O(n * m)
+
 const solution_brute_force = (
   arr: Array<Array<number>>
-): Array<Array<number>> | number => {
+): Array<Array<number>> => {
   let rowZero = new Set<number>();
   let colZero = new Set<number>();
 
@@ -19,16 +22,69 @@ const solution_brute_force = (
       }
     }
   }
-  console.log(rowZero, colZero);
-  console.log(arr);
-  return -1;
+  return arr;
+};
+
+// console.log(
+//   solution_brute_force([
+//     [1, 1, 1, 1],
+//     [1, 0, 1, 1],
+//     [1, 0, 0, 1],
+//     [1, 0, 0, 1],
+//   ])
+// );
+
+// TC = O(n*m)
+// SC = O(1)
+const solution_optimal = (arr: Array<Array<number>>): Array<Array<number>> => {
+  let col0 = 1;
+  // Mark the first index of each row and column => 0 if there is a 0 at any index for that particular row or column. NOTE: if the first column or 0th index column has zero in it, mark col0 = 0;
+  for (let row = 0; row < arr.length; row++) {
+    for (let col = 0; col < arr[row].length; col++) {
+      if (arr[row][col] === 0) {
+        arr[row][0] = 0;
+        if (col === 0) {
+          col0 = 0;
+        } else {
+          arr[0][col] = 0;
+        }
+      }
+    }
+  }
+
+  // Apart from 0th row and 0th column mark every element to zero if [row][0] === 0 || [0][col] === 0
+  for (let row = 1; row < arr.length; row++) {
+    for (let col = 1; col < arr[row].length; col++) {
+      if (arr[row][col] !== 0) {
+        if (arr[row][0] === 0 || arr[0][col] === 0) {
+          arr[row][col] = 0;
+        }
+      }
+    }
+  }
+
+  // If [0][0] => 1st elemt is zero, the first wor will be zero.
+  if (arr[0][0] === 0) {
+    for (let col = 0; col < arr[0].length; col++) {
+      arr[0][col] = 0;
+    }
+  }
+
+  // If variable col0 = 0, the first column will be zero.
+  if (col0 === 0) {
+    for (let row = 0; row < arr.length; row++) {
+      arr[row][0] = 0;
+    }
+  }
+
+  return arr;
 };
 
 console.log(
-  solution_brute_force([
+  solution_optimal([
     [1, 1, 1, 1],
     [1, 0, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 1],
   ])
 );
