1911dfd4495f6fbad1bc828cf6e7cc7945f479d8
Devansh27112001 <dkansara2711@gmail.com>
2025-07-18 12:24:52 -0400

    --brute_force and --optimal

---
+++ numOfSubarraysWithSumK.ts
@@ -0,0 +1,39 @@
+// Generating all sub-arrays
+const solution_brute_force = (arr: Array<number>, k: number): number => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum === k) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+// console.log(solution_brute_force([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3));
+
+// The solution will work for zeros, -ves and +ves as well.
+// TC = O(n)
+// SC = O(n)
+// The concept is same as the longestSubarray - prefix Sum but the logic is different.
+// Key difference to remember: We store <prefixSum, count for that sum> instead of <perfixSum,index>
+const solution_optimal = (arr: Array<number>, k: number): number => {
+  let count = 0;
+  let sum = 0;
+  let sumMap = new Map<number, number>();
+  sumMap.set(sum, 1);
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sumMap.get(sum - k)) {
+      count += sumMap.get(sum - k)!;
+    }
+
+    sumMap.set(sum, (sumMap.get(sum) || 0) + 1);
+  }
+  return count;
+};
+
+console.log(solution_optimal([1, 2, 3, -3, 1, 1, 1, 4, 2, -3], 3));
