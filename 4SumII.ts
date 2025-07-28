ce16654dee02b10940dc187452bd66fe6d7abdd0
Devansh27112001 <dkansara2711@gmail.com>
2025-07-28 18:27:40 -0400

    --optimal

--- 4SumII.ts
+++ 4SumII.ts
@@ -2,6 +2,8 @@
 // 0 <= i, j, k, l < n;
 // nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0;
 
+// TC = O(n ^ 4)
+// SC = O(1)
 const solution_brute_force = (
   nums1: Array<number>,
   nums2: Array<number>,
@@ -25,4 +27,38 @@ const solution_brute_force = (
   return count;
 };
 
-console.log(solution_brute_force([0], [0], [0], [0]));
+// console.log(solution_brute_force([0], [0], [0], [0]));
+
+// TC = O(n ^ 2)
+// SC = O(n ^ 2)
+// The arrays are of n length. You can add upto n * n unique entries in the map while doing the first nested loop. For example, let a = [1,2] and b = [4,6]. The keys will be 4,7,6,8.
+const solution_optimal = (
+  nums1: Array<number>,
+  nums2: Array<number>,
+  nums3: Array<number>,
+  nums4: Array<number>
+): number => {
+  let count = 0;
+  let hashMap = new Map<number, number>();
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      let curr = nums1[i] + nums2[j];
+
+      hashMap.set(curr, (hashMap.get(curr) || 0) + 1);
+    }
+  }
+
+  for (let i = 0; i < nums3.length; i++) {
+    for (let j = 0; j < nums4.length; j++) {
+      let curr = nums3[i] + nums4[j];
+
+      if (hashMap.has(-curr)) {
+        count += hashMap.get(-curr)!;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(solution_optimal([2, 2], [2, 2], [-2, -2], [-2, -2]));
