6213b1614fd6906a3d7804d7a4b4d10f6a87e008
Devansh27112001 <dkansara2711@gmail.com>
2025-07-27 20:15:44 -0400

    --optimal

--- 4Sum.ts
+++ 4Sum.ts
@@ -28,3 +28,41 @@ const solution_brute_force = (
   return solution;
 };
 // console.log(solution_brute_force([1, 0, -1, 0, -2, 2], 0));
+
+const solution_optimal = (
+  nums: Array<number>,
+  target: number
+): Array<Array<number>> => {
+  const solution: Array<Array<number>> = [];
+
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) {
+        continue;
+      }
+      let low = j + 1;
+      let high = nums.length - 1;
+      while (low < high) {
+        const sum = nums[i] + nums[j] + nums[low] + nums[high];
+        if (sum === target) {
+          solution.push([nums[i], nums[j], nums[low], nums[high]]);
+          low++;
+          high--;
+          while (low < high && nums[low] === nums[low - 1]) low++;
+          while (low < high && nums[high] === nums[high + 1]) high--;
+        } else if (sum < target) {
+          low++;
+        } else {
+          high--;
+        }
+      }
+    }
+  }
+  return solution;
+};
+
+console.log(solution_optimal([1, 0, -1, 0, -2, 2], 0));
