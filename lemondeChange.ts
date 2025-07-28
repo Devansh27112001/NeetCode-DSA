7423f42da2b01e96c2b7e217351e8001ac1709ff
Devansh27112001 <dkansara2711@gmail.com>
2025-07-28 18:28:15 -0400

    descrition added

--- lemondeChange.ts
+++ lemondeChange.ts
@@ -1,3 +1,9 @@
+// At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you and order one at a time (in the order specified by bills). Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+
+// Note that you do not have any change in hand at first.
+
+// Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
+
 const solution_optimal = (bills: Array<number>): boolean => {
   let count5: number = 0;
   let count10: number = 0;
