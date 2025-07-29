e50cf690a49fb5295ae583d03eb89fc6cd9d78b7
Devansh27112001 <dkansara2711@gmail.com>
2025-07-29 15:08:23 -0400

    --opimal_better

--- noOfPairsWithKDifference.ts
+++ noOfPairsWithKDifference.ts
@@ -30,3 +30,25 @@ const solution_optimal = (nums: Array<number>, k: number): number => {
   }
   return count;
 };
+
+const solution_optimal_2 = (nums: Array<number>, k: number): number => {
+  let count = 0;
+
+  if (k === 0) {
+    let freqCount = new Map<number, number>();
+    nums.forEach((num) => freqCount.set(num, (freqCount.get(num) || 0) + 1));
+    for (const value of freqCount.values()) {
+      value > 1 && count++;
+    }
+  } else {
+    const numSet = new Set<number>(nums);
+    for (const num of numSet) {
+      if (numSet.has(num + k)) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(solution_optimal_2([3, 1, 4, 1, 5], 2));
