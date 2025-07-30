d3055a695d70762b09ffd7432c5d6454219ad6c3
Devansh27112001 <dkansara2711@gmail.com>
2025-07-30 17:14:56 -0400

    --optimal and --brute-force

---
+++ subArraysSumDivsibleByK.ts
@@ -0,0 +1,37 @@
+// Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+// A subarray is a contiguous part of an array.
+const solution_bruteforce = (nums: Array<number>, k: number): number => {
+  let count: number = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+// console.log(solution_bruteforce([4, 5, 0, -2, -3, 1], 5));
+
+const solution_optimal = (nums: Array<number>, k: number): number => {
+  let count = 0;
+  let remFreq = new Map<number, number>();
+  remFreq.set(0, 1);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    let remainder = sum % k;
+    let prev_reamin_occu = remFreq.get(remainder) || 0;
+    count += prev_reamin_occu;
+    remFreq.set(remainder, (remFreq.get(remainder) || 0) + 1);
+  }
+  count;
+  return count;
+};
+
+solution_optimal([4, 5, 0, -2, -3, 1], 5);
