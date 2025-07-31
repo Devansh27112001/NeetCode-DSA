1a702adbdc9a5461497922c223cae06dfb81b828
Devansh27112001 <dkansara2711@gmail.com>
2025-07-31 09:50:20 -0400

    --optimal

---
+++ noOfSubarraysWithXORk.ts
@@ -0,0 +1,17 @@
+const solution_optimal = (nums: Array<number>, k: number): number => {
+  let count = 0;
+  let xor = 0;
+  // We are string the preXOR and it's frequency.
+  let xorMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    xor ^= nums[i];
+    count += xorMap.get(xor) || 0;
+    if (xor === k) {
+      count++;
+    }
+    xorMap.set(xor, (xorMap.get(xor) || 0) + 1);
+  }
+  return count;
+};
+
+console.log(solution([4, 2, 2, 6, 4], 6));
