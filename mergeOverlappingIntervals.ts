c09ca9515291e45e8b72724af563a32a0aa771d3
Devansh27112001 <dkansara2711@gmail.com>
2025-08-02 11:07:15 -0400

    --brute-force and optimal

--- mergeOverlappingIntervals.ts
+++ mergeOverlappingIntervals.ts
@@ -1,13 +1,9 @@
-const solution = (intervals: Array<Array<number>>): Array<Array<number>> => {
+const solution_bruteforce = (
+  intervals: Array<Array<number>>
+): Array<Array<number>> => {
   const solution: Array<Array<number>> = [];
 
-  intervals.sort((a, b) => {
-    if (a[0] !== b[0]) {
-      return a[0] - b[0];
-    } else {
-      return a[1] - b[1];
-    }
-  });
+  intervals.sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]));
 
   for (let i = 0; i < intervals.length; i++) {
     let start = intervals[i][0];
@@ -23,21 +19,56 @@ const solution = (intervals: Array<Array<number>>): Array<Array<number>> => {
       let start_2 = intervals[j][0];
       let end_2 = intervals[j][1];
       if (start_2 <= end) {
-        if (end < end_2) {
-          end = end_2;
-        }
+        end = Math.max(end, end_2);
       } else {
         break;
       }
     }
     solution.push([start, end]);
   }
-  console.log(solution);
-  return [];
+  return solution;
+};
+
+// console.log(
+//   solution_bruteforce([
+//     [1, 3],
+//     [2, 6],
+//     [8, 9],
+//     [9, 11],
+//     [8, 10],
+//     [2, 4],
+//     [15, 18],
+//     [16, 17],
+//   ])
+// );
+
+const solution_better = (
+  intervals: Array<Array<number>>
+): Array<Array<number>> => {
+  const solution: Array<Array<number>> = [];
+
+  intervals.sort((a, b) => (a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]));
+  intervals;
+  let start = intervals[0][0];
+  let end = intervals[0][1];
+  for (let i = 1; i < intervals.length; i++) {
+    let curr_start = intervals[i][0];
+    let curr_end = intervals[i][1];
+
+    if (curr_start <= end) {
+      end = Math.max(end, curr_end);
+    } else {
+      solution.push([start, end]);
+      start = curr_start;
+      end = curr_end;
+    }
+  }
+  solution.push([start, end]);
+  return solution;
 };
 
 console.log(
-  solution([
+  solution_better([
     [1, 3],
     [2, 6],
     [8, 9],
