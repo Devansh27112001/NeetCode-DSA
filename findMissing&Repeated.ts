6fade496fe8f4a2339d6a652a7428fc1cdc3cf50
Devansh27112001 <dkansara2711@gmail.com>
2025-08-06 18:20:18 -0400

    --optimal approaches

--- findMissing&Repeated.ts
+++ findMissing&Repeated.ts
@@ -27,3 +27,70 @@ console.log(
     [3, 4, 6],
   ])
 );
+
+// TC = O(n^2)
+// SC = O(1)
+const solution_optimal = (grid: Array<Array<number>>): Array<number> => {
+  let n = grid.length * grid.length;
+  let actualSum = (n * (n + 1)) / 2;
+  let actualSqSum = (n * (n + 1) * (2 * n + 1)) / 6;
+
+  let expSum = 0;
+  let expSqSum = 0;
+
+  for (let row of grid) {
+    for (let num of row) {
+      expSum += num;
+      expSqSum += num * num;
+    }
+  }
+
+  // Let a be missing number and b = duplicate number
+  // diff = a - b and diff = actualSum - expSum
+  // a - b
+  let diff = actualSum - expSum;
+  // sq_diff = a^2 - b^2 and sq_diff = expSqSum - actualSqSum => (a-b)(a+b) = sq_diff => a + b = sq_diff / (a-b)
+  let sumAB = (actualSqSum - expSqSum) / diff;
+
+  let a = (diff + sumAB) / 2;
+  let b = a - diff;
+  return [b, a];
+};
+
+// console.log(
+//   solution_optimal([
+//     [9, 1, 7],
+//     [8, 9, 2],
+//     [3, 4, 6],
+//   ])
+// );
+
+const solution_optimal_2 = (grid: Array<Array<number>>): Array<number> => {
+  // let a = duplicate and b = missing
+  let n = grid.length ** 2;
+  let a: number = -1,
+    b: number = -1;
+  let numSet = new Set<number>();
+  let actualSum = 0;
+
+  for (const row of grid) {
+    for (const num of row) {
+      actualSum += num;
+      if (numSet.has(num)) {
+        a = num;
+      }
+      numSet.add(num);
+    }
+  }
+
+  let expSum = (n * (n + 1)) / 2;
+  b = expSum + a - actualSum;
+  return [a, b];
+};
+
+console.log(
+  solution_optimal_2([
+    [1, 3],
+    [2, 2],
+  ])
+);
