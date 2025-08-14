358338c071fbe53ba0dd6024953d08b482af9936
Devansh27112001 <dkansara2711@gmail.com>
2025-08-14 10:04:54 -0400

    --optimal and --better

--- singleElementInASortedArray.ts
+++ singleElementInASortedArray.ts
@@ -31,7 +31,42 @@ const solution_better = (nums: Array<number>): number => {
   return -1;
 };
 
+const solution_better_2 = (nums: Array<number>): number => {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 == 0) {
+      sum += nums[i];
+    } else {
+      sum -= nums[i];
+    }
+  }
+  return sum;
+};
+console.log(solution_better_2([3, 3, 7, 7, 10, 11, 11]));
+
+// It was given that the solution needs to be in O(logn) => Binary search
+// Observe diff. test cases to find out about the mid%2 and whether to go left side or right side.
 const solution_optimal = (nums: Array<number>) => {
   let low = 0;
   let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid - 1] !== nums[mid] && nums[mid + 1] !== nums[mid]) {
+      return nums[mid];
+    } else if (mid % 2 === 0) {
+      if (nums[mid - 1] === nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    } else {
+      if (nums[mid - 1] === nums[mid]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
 };
