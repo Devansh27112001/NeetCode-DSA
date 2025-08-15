132f39cb157624ecafb3db15358fcda47f260600
Devansh27112001 <dkansara2711@gmail.com>
2025-08-15 17:20:45 -0400

    --optimal

---
+++ summaryRanges.ts
@@ -0,0 +1,28 @@
+// You are given a sorted unique integer array nums.
+// A range [a,b] is the set of all integers from a to b (inclusive).
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+// "a->b" if a != b
+// "a" if a == b
+
+const solution_optimal = (nums: Array<number>): Array<string> => {
+  let res: Array<string> = [];
+  let start = nums[0];
+  let end = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - 1 === end) {
+      end = nums[i];
+    } else {
+      start === end ? res.push(`${start}`) : res.push(`${start}->${end}`);
+      start = nums[i];
+      end = nums[i];
+    }
+  }
+
+  start === end ? res.push(`${start}`) : res.push(`${start}->${end}`);
+  return res;
+};
+
+console.log(solution_optimal([0, 2, 3, 4, 6, 8, 9]));
