0c7e3df0cdee3dcf04b6a6b5b3e0d1f7e54d4505
Devansh27112001 <dkansara2711@gmail.com>
2025-08-15 13:46:31 -0400

    --optimal

---
+++ containsDuplicateII.ts
@@ -0,0 +1,14 @@
+// DESCRIPTION : Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+const solution_optimal = (nums: Array<number>, k: number): boolean => {
+  let numMap = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numMap.has(nums[i]) && i - numMap.get(nums[i])! <= k) {
+      return true;
+    } else {
+      numMap.set(nums[i], i);
+    }
+  }
+  return false;
+};
