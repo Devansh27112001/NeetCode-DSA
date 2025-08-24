8ba5c18feb8770dbfd9f4cbbc56e98170cdb4236
Devansh27112001 <dkansara2711@gmail.com>
2025-08-24 09:18:29 -0400

    --optimal different approach

--- groupAnagrams.ts
+++ groupAnagrams.ts
@@ -27,3 +27,26 @@ function isAnagramsByHashMap(strs: string[]): Array<Array<string>> {
 
   return Array.from(anagramMap.values());
 }
+
+// TC = O(m * n)
+// SC = O(m * n)
+const solution_optimal_2 = (strs: Array<string>): Array<Array<string>> => {
+  let ans: { [key: string]: Array<string> } = {};
+
+  for (const str of strs) {
+    let count = new Array(26).fill(0);
+
+    for (const char of str) {
+      count[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
+
+    let key = count.join("#");
+    if (!ans[key]) {
+      ans[key] = [];
+    }
+
+    ans[key].push(str);
+  }
+  return Object.values(ans);
+};
+console.log(solution_optimal_2(["eat", "tea", "tan", "ate", "nat", "bat"]));
