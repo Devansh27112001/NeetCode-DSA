1b7f98c873fe0280bb09356bb06e4c4044e2c089
Devansh27112001 <dkansara2711@gmail.com>
2025-09-16 00:09:23 -0400

    brute_force

---
+++ teemoAttacking.ts
@@ -0,0 +1,20 @@
+/*Teemo Attacking 
+
+Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+
+You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+
+Return the total number of seconds that Ashe is poisoned.
+*/
+const solutin_brute_force = (nums: Array<number>, duration: number): number => {
+  const count = 0;
+  let pSet = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < duration; j++) {
+      pSet.add(nums[i] + j);
+    }
+  }
+  return pSet.size;
+};
+
+console.log(solutin_brute_force([1, 2], 2));
