98783c17b3f3359e5237db48620c38311f7e4aeb
Devansh27112001 <dkansara2711@gmail.com>
2025-10-03 00:23:39 -0400

    Small changes

--- canPlaceFlowers.ts
+++ canPlaceFlowers.ts
@@ -4,7 +4,7 @@ You have a long flowerbed in which some of the plots are planted, and some are n
 Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
 */
 
-const solution = (flowerbed: Array<number>, n: number): boolean => {
+const solution_optimal = (flowerbed: Array<number>, n: number): boolean => {
   for (let i = 0; i < flowerbed.length; i++) {
     if (flowerbed[i] === 0) {
       if (i > 0 && i < flowerbed.length - 1) {
@@ -28,4 +28,4 @@ const solution = (flowerbed: Array<number>, n: number): boolean => {
   return n === 0;
 };
 
-console.log(solution([1, 0, 0, 0, 0, 1], 2));
+console.log(solution_optimal([1, 0, 0, 0, 0, 1], 2));
