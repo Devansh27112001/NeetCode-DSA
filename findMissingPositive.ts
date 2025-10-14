9ec3d5448e93de7d66bb0b8664e7b274b4698ff5
Devansh27112001 <dkansara2711@gmail.com>
2025-10-14 11:34:48 -0400

    --optimal

--- findMissingPositive.ts
+++ findMissingPositive.ts
@@ -4,21 +4,23 @@ Given an unsorted integer array nums. Return the smallest positive integer that 
 You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
 */
 
-const solution_brute_force = (nums: number[]): number => {
-  let found = false;
-  for (let i = 1; i <= nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i === nums[j]) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) return i;
-  }
-  return 0;
-};
+// The brute force way would be to sort the input array and check for the smallest +ve integer starting from 1 and traversing the sorted array.
 
-console.log(solution_brute_force([0, 1, 2]));
+// TC = O(n log n)
+// SC = O(1)
+const solution_brute_force = (nums: number[]): number => {
+  let smallest = 1;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === smallest) {
+      smallest++;
+    } else if (nums[i] > smallest) {
+      return smallest;
+    }
+  }
+  return smallest;
+};
+// console.log(solution_brute_force([0, 1, 2]));
 
 // TC = O(n)
 // SC = O(n)
@@ -32,3 +34,34 @@ const solution_better = (nums: number[]): number => {
   return i;
 };
 // console.log(solution_better([7, 8, 9, 11, 12]));
+
+// Optimal Approach
+// TC = O(n)
+// SC = O(1)
+const solution_optimal = (nums: number[]): number => {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) {
+      nums[i] = 0;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (index >= 0 && index <= nums.length - 1) {
+      if (nums[index] > 0) {
+        nums[index] = -nums[index];
+      } else if (nums[index] === 0) {
+        nums[index] = -(nums.length + 1);
+      }
+    }
+  }
+
+  console.log(nums);
+  for (let i = 1; i < nums.length + 1; i++) {
+    if (nums[i - 1] >= 0) {
+      return i;
+    }
+  }
+  return nums.length + 1;
+};
+console.log(solution_optimal([1, 2, 0]));
